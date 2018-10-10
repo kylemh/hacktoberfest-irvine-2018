2aeadc6ef910051b93a733385e2e4e0af8098b5f
Kyle Holmberg <inbox@kylemh.com>
2018-10-09 23:08:39 -0700

    Initial MDX Deck commit with custom theme and intro slides

---
+++ hackdeck/theme.js
@@ -0,0 +1,17 @@
+import theme from 'mdx-deck/themes';
+
+export default {
+  ...theme,
+  font: 'Ubuntu, sans-serif',
+  colors: {
+    text: '#fafafa',
+    background: '#411e2f',
+    link: '#21ffff',
+    heading: '#ff9a56',
+  },
+  // Customize your presentation theme here.
+  //
+  // Read the docs for more info:
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+};
